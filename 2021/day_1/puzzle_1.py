281bfa675b21f3586ddc557bae633883dc95c140
Jack Hill <jackhill1414@gmail.com>
2021-12-06 23:47:29 +0000

    cleaned up context manager

--- 2021/day_1/puzzle_1.py
+++ 2021/day_1/puzzle_1.py
@@ -1,14 +1,10 @@
 
 with open(r'2021/day_1/SonarSweepInput.txt', 'r') as sonar_data:
-    depths = sonar_data.readlines()
-    depths = [depth.strip() for depth in depths]
+    depths = [int(depth.strip()) for depth in sonar_data.readlines()]
 
     counter = 0
     for i, depth in enumerate(depths):
-        if int(depth) > int(depths[i-1]):
+        if depth > depths[i-1]:
             counter += 1
 
-print(counter)   
-    
-
-
+print(counter) 
