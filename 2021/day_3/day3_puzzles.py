1b0a08b9b68a2537712814e0fc6df2c37f9617b6
Jack Hill <jackhill1414@gmail.com>
2021-12-12 02:06:18 +0000

    actually got somewhere, that took me far too long, we live and learn

--- 2021/day_3/day3_puzzles.py
+++ 2021/day_3/day3_puzzles.py
@@ -1,28 +1,58 @@
 
-from typing import final
+from os import getgrouplist
 
 
 with open(r'2021/day_3/binaryinput.txt', 'r') as f:
     binaries = [line.strip() for line in f.readlines()]
 
 
-gamma_rate_bin = ''
-epsilon_bin = ''
-for i in range(0, 12):
-    val_count = 0
-    for binary in binaries:
-        if binary[i] == "1":
-            val_count += 1
+def get_power_consum(binaries):
+    gamma_rate_bin = ''
+    epsilon_bin = ''    
+
+    for i in range(12):
+        val_count = 0
+        for binary in binaries:
+            if binary[i] == "1":
+                val_count += 1
+            else:
+                val_count -= 1
+        if val_count > 0:
+            gamma_rate_bin += "1"
+            epsilon_bin += "0"
         else:
-            val_count -= 1
-    if val_count > 0:
-        gamma_rate_bin += "1"
-        epsilon_bin += "0"
+            gamma_rate_bin += "0"
+            epsilon_bin += "1"
+    
+    return int(gamma_rate_bin, 2) * int(epsilon_bin, 2)
+
+
+def get_life_support_rating(binaries, bit_num):
+    most_common = 0 
+
+    if len(binaries) == 1:
+        return binaries[0]
+
     else:
-        gamma_rate_bin += "0"
-        epsilon_bin += "1"
+        for binary in binaries:
+            if binary[bit_num] > "0":
+                most_common += 1
+            else:
+                most_common -= 1
 
-final_gamma = int(gamma_rate_bin, 2)
-final_eps = int(epsilon_bin, 2)
+        if most_common >= 0:
+            binaries = [binary for binary in binaries if binary[bit_num] == "1"]
+        else:
+            binaries = [binary for binary in binaries if binary[bit_num] == "0"]
+        
+    bit_num += 1
 
-print(final_gamma * final_eps)
+    return get_life_support_rating(binaries, bit_num)    
+
+
+
+    # return int(oxy_rating, 2) * int(co2_rating)
+
+# print(get_power_consum(binaries))
+
+print(get_life_support_rating(binaries, 0))
