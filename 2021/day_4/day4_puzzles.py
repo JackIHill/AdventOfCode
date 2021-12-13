ff01318729e9a2c35ce9e951cc2fab449f602962
Jack Hill <jackhill1414@gmail.com>
2021-12-13 18:02:57 +0000

    updated

--- 2021/day_4/day4_puzzles.py
+++ 2021/day_4/day4_puzzles.py
@@ -1,11 +1,27 @@
 
-import pprint
+from collections import _OrderedDictValuesView
+from os import sched_get_priority_max
+from pprint import pprint
 
 with open('2021/day_4/bingoinput.txt') as f:
     bingo = f.readlines()
 
-boards = [pprint.pprint(bingo[2:])]
-order = [bingo[0].strip()]
+order = [int(i) for i in bingo[0].split(',')]
+boards = []
 
-for board in boards:
-    print(board)
+rows = [[int(i) for i in row.split()] for row in board.split('\n')]
+
+# boards = dict.fromkeys(range(len(boards_nested)))
+# boards = dict(zip(boards, boards_nested))
+
+# print(order)
+
+# for i, board in enumerate(boards_nested):
+#     for line in board:
+#         for n in line:
+#             print(n)
+
+# print(boards_nested)
+
+# print(order)
+print(board)
