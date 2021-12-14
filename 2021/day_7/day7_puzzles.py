8a5ca6688b768e2ad30a21d881877254572f28fa
Jack Hill <jackhill1414@gmail.com>
2021-12-14 02:30:12 +0000

    better solution for part 1

--- 2021/day_7/day7_puzzles.py
+++ 2021/day_7/day7_puzzles.py
@@ -4,13 +4,33 @@ with open('2021/day_7/crabinput.txt') as f:
 
 from statistics import median
 
-median = median(positions)
 
-listies = []
-for position in positions:
-    if position <= median:
-        listies.append(int(median) - position)
-    else:
-        listies.append(position - int(median))
+def crab_part1():
+    medians = median(positions)
+    fuels = []
+    for position in positions:
+        if position <= medians:
+            fuels.append(medians - position)
+        else:
+            fuels.append(position - medians)
 
-print(sum(listies))
+    return sum(fuels)
+
+# print(crab_part1())  
+
+def crab_part2():
+    pos = 0
+    best_fuel = set()
+    while pos < len(positions):
+        for n in range(1, len(positions) + 1):
+            total_fuel = 0
+            for position in positions:
+                fuel = abs(n-position)   
+                total_fuel += fuel                
+
+            best_fuel.add(total_fuel)
+        
+        pos += 1
+        return (min(best_fuel))   
+            
+print(crab_part2())
