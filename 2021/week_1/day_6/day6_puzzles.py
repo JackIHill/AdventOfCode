60d3d3b5de59e0248126f1fdbfe0ff22b788de7c
Jack Hill <jackhill1414@gmail.com>
2021-12-18 18:08:16 +0000

    small changes to comment order, variable creation order for consistency

--- 2021/week_1/day_6/day6_puzzles.py
+++ 2021/week_1/day_6/day6_puzzles.py
@@ -24,15 +24,16 @@ def how_many_fish(days):
 def how_many_fish_better(days):
     fish_counts = {}
     # set dict keys to number of days until birth
-    for key in range(0, 9):
-        fish_counts[key] = 0
+    for val in range(0, 9):
+        fish_counts[val] = 0
     # set dict values to number of fish for each of those days
     for fish in fishies:
         fish_counts[fish] += 1
 
     for _ in range(days):
-        # get starting number of fish at 0th day until birth, and initialise that to 0
-        # each new day (as 'a 0 becomes a 6 and adds a new 8').
+        # get starting number of fish at their 0th day until birth
+        # and initialise that to 0 each new day 
+        # ('a 0 becomes a 6 and adds a new 8').
         num_zero = fish_counts[0]
         fish_counts[0] = 0
 
@@ -42,10 +43,11 @@ def how_many_fish_better(days):
             fish_counts[i] = 0
 
         # all 0's become 6's and 8's. 
-        fish_counts[8] += num_zero
         fish_counts[6] += num_zero
+        fish_counts[8] += num_zero
+        
 
     return sum(fish_counts.values())
 
-print(how_many_fish(80))
-# print(how_many_fish_better(256))
+# print(how_many_fish(80))
+print(how_many_fish_better(256))
