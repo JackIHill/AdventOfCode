f8c36b7650212459b408a9d8ef3c6ce2243830ae
Jack Hill <jackhill1414@gmail.com>
2021-12-23 20:41:17 +0000

    split script into functions

--- 2021/week_2/day_14/day14_puzzles.py
+++ 2021/week_2/day_14/day14_puzzles.py
@@ -4,30 +4,34 @@ from collections import Counter
 with open('2021/week_2/day_14/polymerinput.txt') as f:
     polyinputs = [f.strip() for f in f.readlines()]
 
-poly = polyinputs[0]
+def base_poly_and_pairs():
+    poly = polyinputs[0]
 
-keys = [pair[0:2] for pair in polyinputs[2:]]
-vals = [pair[6] for pair in polyinputs[2:]]
+    keys = [pair[0:2] for pair in polyinputs[2:]]
+    vals = [pair[6] for pair in polyinputs[2:]]
 
-pairs = {keys[i]: vals[i] for i in range(len(keys))}
+    pairs = {keys[i]: vals[i] for i in range(len(keys))}
 
+    return poly, pairs
 
-for _ in range(12):
-    new_poly = []
-    chunks = [poly[i] + poly[i+1] for i in range(len(poly)-1)]
-    for i, c in enumerate(chunks):
-        for key, value in pairs.items():
-            if c == key:
-                new_poly += c[0] + value
+poly, pairs = base_poly_and_pairs()
 
-    new_poly.append(chunks[-1][1])
-    poly = ''.join(new_poly)
+def pair_insertion(steps, poly, pairs):
+    for _ in range(steps):
+        new_poly = []
+        chunks = [poly[i] + poly[i+1] for i in range(len(poly)-1)]
+        for i, c in enumerate(chunks):
+            for key, value in pairs.items():
+                if c == key:
+                    new_poly += c[0] + value
 
+        new_poly.append(chunks[-1][1])
+        poly = ''.join(new_poly)
 
-element_count = Counter(poly)
-print(element_count.most_common()[0][1] - element_count.most_common()[-1][1])
+    element_count = Counter(poly)
+    return element_count.most_common()[0][1] - element_count.most_common()[-1][1]
 
-
+print(pair_insertion(10, poly, pairs))
 
 
 
